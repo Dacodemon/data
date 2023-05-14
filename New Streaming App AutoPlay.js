33e0933f2279563a62294f6bec1cbdd7763bdded
CG Animation <103148052+Dacodemon@users.noreply.github.com>
2023-05-14 17:35:54 -0500

    Add files via upload

---
+++ New Streaming App AutoPlay.js
@@ -0,0 +1,35 @@
+function autoplay(){
+document.getElementById("sss ct").addEventListener("ended",function(){
+document.getElementById("ams ct").play()
+})
+document.getElementById("ams ct").addEventListener("ended",function(){
+document.getElementById("jm ct").play()
+})
+document.getElementById("jm ct").addEventListener("ended",function(){
+document.getElementById("j ct").play()
+})
+document.getElementById("j ct").addEventListener("ended",function(){
+document.getElementById("mtw ct").play()
+})
+document.getElementById("lam tm").addEventListener("ended",function(){
+document.getElementById("mwy tm").play()
+})
+document.getElementById("lam tm").addEventListener("ended",function(){
+document.getElementById("mwy tm").play()
+})
+document.getElementById("mwy tm").addEventListener("ended",function(){
+document.getElementById("bd tm").play()
+})
+document.getElementById("bd tm").addEventListener("ended",function(){
+document.getElementById("wihpp cc").play()
+})
+document.getElementById("wihpp cc").addEventListener("ended",function(){
+document.getElementById("wwagth cc").play()
+})
+document.getElementById("nbtb cc").addEventListener("ended",function(){
+document.getElementById("gisg pb").play()
+})
+}
+document.getElementById("autoplay").onclick=function(){
+autoplay()
+}
