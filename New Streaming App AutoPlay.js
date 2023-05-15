39d83cbd023f374531c720915d5e722527121c6d
CG Animation <103148052+Dacodemon@users.noreply.github.com>
2023-05-14 23:45:12 -0500

    nsa autoplay

--- New Streaming App AutoPlay.js
+++ New Streaming App AutoPlay.js
@@ -29,6 +29,57 @@ document.getElementById("wwagth cc").play()
 document.getElementById("nbtb cc").addEventListener("ended",function(){
 document.getElementById("gisg pb").play()
 })
+document.getElementById("nbtb cc").addEventListener("ended",function(){
+document.getElementById("gisg pb").play()
+})
+document.getElementById("gisg pb").addEventListener("ended",function(){
+document.getElementById("trust in you bass boosted").play()
+})
+document.getElementById("trust in you bass boosted").addEventListener("ended",function(){
+document.getElementById("acom wwasj").play()
+})
+document.getElementById("acom wwasj").addEventListener("ended",function(){
+document.getElementById("acom dia").play()
+})
+document.getElementById("acom dia").addEventListener("ended",function(){
+document.getElementById("acom jmct").play()
+})
+document.getElementById("acom jmct").addEventListener("ended",function(){
+document.getElementById("acom cdm").play()
+})
+document.getElementById("acom cdm").addEventListener("ended",function(){
+document.getElementById("acom ctcw").play()
+})
+document.getElementById("acom ctcw").addEventListener("ended",function(){
+document.getElementById("acom jckmitm").play()
+})
+document.getElementById("acom jckmitm").addEventListener("ended",function(){
+document.getElementById("acom jdjb").play()
+})
+document.getElementById("acom jdjb").addEventListener("ended",function(){
+document.getElementById("acom wub").play()
+})
+document.getElementById("acom wub").addEventListener("ended",function(){
+document.getElementById("acom glc").play()
+})
+document.getElementById("acom glc").addEventListener("ended",function(){
+document.getElementById("acom ttinat").play()
+})
+document.getElementById("acom ttinat").addEventListener("ended",function(){
+document.getElementById("acom gcbs").play()
+})
+document.getElementById("acom gcbs").addEventListener("ended",function(){
+document.getElementById("acom mib").play()
+})
+document.getElementById("acom mib").addEventListener("ended",function(){
+document.getElementById("acom rseots").play()
+})
+document.getElementById("acom rseots").addEventListener("ended",function(){
+document.getElementById("acom hnhsdm").play()
+})
+document.getElementById("acom hnhsdm").addEventListener("ended",function(){
+document.getElementById("acom cthgiog").play()
+})
 }
 document.getElementById("autoplay").onclick=function(){
 autoplay()
